76ebbf91466c77a7aa7c94ba9e3dbf08fc2441b5
Andrew Wanyama <andywanyama@gmail.com>
2022-05-12 16:12:54 +0300

    Allow learner to choose a difficulty level

--- Multiplication.java
+++ Multiplication.java
@@ -18,20 +18,26 @@ public class Multiplication
     static int responsesCounter = 0,
                correctResponses = 0,
                incorrectResponses = 0;
+    // store difficulty level
+    static int difficultyLevel = 0;
+    static String studentName; // learner's name
 
-    
     public static void main(String[] args)
     {
-        System.out.printf("\t\t%s%n%s%n%s%n%s%n  %s%n  %s%n%n",
+        System.out.printf("Please enter your name: ");
+        studentName = input.nextLine();
+        chooseDifficultyLevel();
+
+        System.out.printf("\t\t\t%s%n%s%n%s%n%s%n  %s%n  %s%n%n",
             "Easy Multiplication",
-            "-------------------------------------------------------",
-            "Enter your answer to the question at the prompt.",
+            "----------------------------------------------------------------",
+            "Type your answer to the question at the prompt then press Enter",
             "Type the end-of-file indicator to terminate the puzzle:",
             "On UNIX/Linux/Mac OS X type <Ctrl> d then press Enter",
             "On Windows type <Ctrl> z then press Enter");
 
         // generate question and prompt for input
-        generateQuestion();
+        generateQuestion(difficultyLevel);
         int answer = 0;
 
         // loop until the end-of-file indicator is entered
@@ -66,12 +72,11 @@ public class Multiplication
                     System.out.println(); // skip a line
                     if (responsesCounter == 10)
                     {
-                        System.out.println("Calculating your performance...");
                         calculatePerformance();
                         break;
                     }
                     else
-                        generateQuestion();           
+                        generateQuestion(difficultyLevel);           
                 }
                 else // if answer is incorrect,
                 {
@@ -79,8 +84,7 @@ public class Multiplication
                     ++incorrectResponses;
                     if (responsesCounter == 10)
                     {
-                        System.out.printf("%s%n%n%s%n",
-                        "Don't give up!", "Calculating your performance...");
+                        System.out.printf("%s%n", "Don't give up!");
                         calculatePerformance();
                         break;
                     }
@@ -116,33 +120,79 @@ public class Multiplication
         } // end while
     } // end main
 
-    // method that generates a new question
-    private static void generateQuestion()
+    private static void chooseDifficultyLevel()
     {
-        // pick random positive one-digit integers
-        number1 = 1 + randomNumbers.nextInt(9);
-        number2 = 1 + randomNumbers.nextInt(9);
+        System.out.printf("%s%n%s%n%s%n", "Choose difficulty level:",
+            "1 - Level 1", "2 - Level 2");
+        while (difficultyLevel == 0)
+        {
+            try
+            {
+                int level = input.nextInt();
+                if (level == 1 || level == 2)
+                    difficultyLevel = level;
+                else
+                    System.out.println(
+                      "Invalid input. Enter 1 for Level 1 or 2 for Level 2:");
+            }
+            catch (InputMismatchException inputMismatchException)
+            {
+                System.err.printf("Exception: %s%n", inputMismatchException);
+                
+                input.nextLine(); // discard input so user can try again
+                System.out.println(
+                    "You must enter an integer. Please try again: ");
+            }
+            catch (Exception exception)
+            {
+                exception.printStackTrace();
+                System.exit(1);
+            }
+        }
+    } // end method chooseDifficultyLevel
+
+    // method that generates a new question
+    private static void generateQuestion(int difficultLevel)
+    {
+        switch (difficultLevel)
+        {
+            // difficult level 1: use one-digit random positive integers
+            case 1:
+                number1 = 1 + randomNumbers.nextInt(9);
+                number2 = 1 + randomNumbers.nextInt(9);
+                break;
+            // difficult level 2: random positive ints as large as two digits
+            case 2:
+                number1 = 3 + randomNumbers.nextInt(10);
+                number2 = 3 + randomNumbers.nextInt(10);
+                break;
+        }
         // multiply the two integers then store result in product
         product = number1 * number2;
 
         // prompt learner with a multiplication question
         System.out.printf("How much is %d times %d? ", number1, number2);
-    }
+    } // end method generateQuestion
 
     // after 10 answers, calculate the percentage of correct responses
     private static void calculatePerformance()
     {
-        System.out.println();
+        System.out.printf("%n%s%n%s%n",
+            "Your Performance", "----------------");
         System.out.printf("Correct answers: %d%n", correctResponses);
         System.out.printf("Incorrect answers: %d%n", incorrectResponses);
 
         double performance = (double) correctResponses / responsesCounter * 100;
         System.out.printf("Percentage of correct answers: %.2f%s%n",
             performance, "%");
-            
+        
+        System.out.println();
         if (performance < 75)
-            System.out.println("Please ask your teacher for extra help.");
+            System.out.printf("%s%n%s%n", "Nice try, " + studentName + "!",
+                "Please ask your teacher for extra help.");
         else
-            System.out.println("Congratulations, you are ready to go to the next level!");
-    }
+            System.out.printf("%s%n%s%n",
+                "Congratulations, " + studentName + "!",
+                "You are ready to go to Level " + (++difficultyLevel) + ".");
+    } // end method calculatePerformance
 } // end class Multiplication
